59e4a0f9f79959af54a071a6ea1b1d795f195fb9
Lukasz Ozga <lukaszozga@Lukaszs-MacBook-Pro.local>
2022-01-09 22:54:44 +0000

    Containerize application, update README.md

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # personal-blog
+Personal blog application written in Node.js and Express.js. Application uses EJS as view engine.
+
+## Development setup
+For convenience, application has been containerized. This makes setting up of development environment much easier:
+
+* Create `.env` file in root directory with the following enviroment variables (see `.example-env`):
+    * PORT=<port_an_application_will_listen_on>
+    * DB_URL=<mongodb_url> (default: DB_URL=mongodb://mongo:27017/blogDB)
+* Navigate to `docker` directory:
+    ```
+    cd docker
+    ```
+* Run docker-compose command to bring the service up and down:
+    ```
+    docker-compose up
+    ```
+    ```
+    docker-compose down
+    ```
+Root directory is automatically mapped to `/app` directory inside container which allows to make chnages and get instant feedback:
+
+* Any chnages to app.js file will trigger restart of node application
+* Any template changes require refresh in a browser to become visible
--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 // Configure MongoDB connection
-mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+mongoose.connect(process.env.DB_URL, {useNewUrlParser: true});
 
 // Create post schema and model
 const postSchema = {
@@ -82,6 +82,6 @@ app.get("/posts/:postID", function(req, res){
   });
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(process.env.PORT, function() {
+  console.log(`Server started on port ${process.env.PORT}`);
 });
